7cf22b7773877759bf2831f3fc1e1c125758c888
Dragonico1 <128107832+Dragonico1@users.noreply.github.com>
2024-10-27 19:04:51 -0500

    Delete ICurso.java

--- ICurso.java
+++
@@ -1,9 +0,0 @@
-package Taller3;
-public interface ICurso  {
-    public void ingresarEstudiante(Estudiante estudiante);
-    public Estudiante buscarEstudiante(String nombre);
-    public void eliminarEstudiante(String nombre);
-    public Estudiante actualizarEstudiante(String nombreActu, String in_nombre, int in_edad, String in_matricula);
-    public void ordenarEstudiante();
-    
-}
